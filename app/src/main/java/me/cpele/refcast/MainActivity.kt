ca55c9456e4c514e9d1fb5adb3faa784fde7b7b0
cpcp <geekarist@users.noreply.github.com>
2017-11-28 07:29:26 +0100

    Call weather web service

--- app/src/main/java/me/cpele/refcast/MainActivity.kt
+++ app/src/main/java/me/cpele/refcast/MainActivity.kt
@@ -4,6 +4,12 @@ import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import me.cpele.refcast.databinding.ActivityMainBinding
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,6 +18,32 @@ class MainActivity : AppCompatActivity() {
 
         val binding: ActivityMainBinding =
                 DataBindingUtil.setContentView(this, R.layout.activity_main)
-        binding.data = MainData("21°")
+
+        val retrofit = Retrofit.Builder()
+                .baseUrl("http://api.openweathermap.org")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+
+        val service = retrofit.create(WeatherService::class.java)
+
+        service.findWeather().enqueue(object : Callback<Weather> {
+
+            override fun onFailure(call: Call<Weather>?, t: Throwable?) {
+                TODO("not implemented")
+            }
+
+            override fun onResponse(call: Call<Weather>?, response: Response<Weather>?) {
+                response?.body()?.main?.temp?.apply { binding.data = MainData(toString()) }
+            }
+        })
     }
 }
+
+interface WeatherService {
+    @GET("/data/2.5/weather?q=London,uk")
+    fun findWeather(): Call<Weather>
+}
+
+data class Weather(val main: WeatherMain)
+
+data class WeatherMain(val temp: Double)
